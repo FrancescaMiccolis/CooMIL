8ba28b108f8386944570793279969c66300fc749
giuliacorso <giulia.corso.mo.99@gmail.com>
2025-03-19 16:28:29 +0100

    modify launch file

--- launch.sh
+++ launch.sh
@@ -12,13 +12,7 @@ touch /homes/gcorso/outputs/output_coomil.txt
 touch /homes/gcorso/errors/error_coomil.txt
 cd /homes/gcorso/Conslide_dev/
 
-base="
---model, conslide, --dataset, seq-wsi, --exp_desc, conslide, --buffer_size, 1100, --alpha, 0.2, --beta, 0.2
-"
-
-srun -N1 -n1 --gres=gpu:1 python -u -m /homes/gcorso/Conslide_dev/utils/main ${base} &
-
-wait
+CUDA_VISIBLE_DEVICES=0 python -u /homes/gcorso/Conslide_dev/utils/main.py --model conslide --dataset seq-wsi --exp_desc conslide --buffer_size 1100 --alpha 0.2 --beta 0.2 --n_epochs 5
 
 
 
